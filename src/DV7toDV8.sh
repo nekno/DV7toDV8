70b3b04a5a465c753543d26a5fadd145c5f495f9
nekno <nekno@users.noreply.github.com>
2023-06-02 23:29:07 -0500

    Initial commit of DV7toDV8 script

---
+++ src/DV7toDV8.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Only needed when relying on a local install of mkvtoolnix
+# which mkvmerge >/dev/null
+# if [[ $? == 1 ]]
+# then
+#     echo "Run 'brew install mkvtoolnix' to install mkvmerge"
+#     exit 1
+# fi
+# which mkvextract >/dev/null
+# if [[ $? == 1 ]]
+# then
+#     echo "Run 'brew install mkvtoolnix' to install mkvextract"
+#     exit 1
+# fi
+
+targetDir=$1
+
+if [[ $# == 0 ]]
+then
+    echo "Please provide a directory: "
+    read targetDir
+fi
+
+if [[ ! -d $targetDir ]]
+then
+    echo "Directory not found: '$targetDir'"
+    exit 1
+fi
+
+echo "Processing directory: '$targetDir'"
+
+# Get the script's directory path; do this before pushing the targetDir
+scriptDir=`dirname "$(readlink -f "$0")"`
+
+pushd "$targetDir"
+
+# Get the subdirectory paths
+toolsPath=$scriptDir/tools
+configPath=$scriptDir/config
+
+# Reference the dovi_tool, mkvextract, and mkvmerge executables and the JSON file in their respective subdirectories
+doviToolPath=$toolsPath/dovi_tool
+mkvextractPath=$toolsPath/mkvextract
+mkvmergePath=$toolsPath/mkvmerge
+jsonFilePath=$configPath/DV7toDV8.json
+
+for mkvFile in "$targetDir"/*.mkv
+do
+    mkvBase=`basename "$mkvFile" .mkv`
+
+	echo "Demuxing DV7 BL+EL+RPU HEVC from MKV..."
+    "$mkvextractPath" "$mkvFile" tracks 0:"$mkvBase.DV7.BL_EL_RPU.hevc"
+	
+	echo "Demuxing DV7 EL+RPU HEVC..."
+	"$doviToolPath" demux --el-only "$mkvBase.DV7.BL_EL_RPU.hevc" -e "$mkvBase.DV7.EL_RPU.hevc"
+	
+	echo "Converting DV7 BL+EL+RPU to DV8 BL+RPU..."
+    "$doviToolPath" --edit-config "$jsonFilePath" convert --discard "$mkvBase.DV7.BL_EL_RPU.hevc" -o "$mkvBase.DV8.BL_RPU.hevc"
+	
+	echo "Deleting DV7 BL+EL+RPU HEVC..."
+	rm "$mkvBase.DV7.BL_EL_RPU.hevc"
+	
+	echo "Extracting DV8 RPU..."
+    "$doviToolPath" extract-rpu "$mkvBase.DV8.BL_RPU.hevc" -o "$mkvBase.DV8.RPU.bin"
+	
+	echo "Plotting L1..."
+    "$doviToolPath" plot "$mkvBase.DV8.RPU.bin" -o "$mkvBase.DV8.L1_plot.png"
+	
+	echo "Remuxing DV8 MKV..."
+    "$mkvmergePath" -o "$mkvBase.DV8.mkv" -D -a en -s en "$mkvFile" "$mkvBase.DV8.BL_RPU.hevc" --track-order 1:0
+	
+	echo "Cleaning up..."
+    rm "$mkvBase.DV8.RPU.bin"
+    rm "$mkvBase.DV8.BL_RPU.hevc"
+done
+
+popd
+echo "Done."
